76c4440824aaae139de585d8c3043c41c47b1786
ranjithkodangil <ranjithkodangil@gmail.com>
2026-04-01 18:14:18 +0530

    fix:updated

--- vite.config.js
+++ vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
 
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://webrtc-server-liard.vercel.app',
         changeOrigin: true,
         secure: false,
       }
