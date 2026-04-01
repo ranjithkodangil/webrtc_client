652f0019580c571190eafcb236e84a69c0473928
ranjithkodangil <ranjithkodangil@gmail.com>
2026-04-01 19:23:34 +0530

    fix:vite-config

--- vite.config.js
+++ vite.config.js
@@ -7,7 +7,7 @@ export default defineConfig(({ mode }) => {
 
   return {
     plugins: [react()],
-    base: process.env.VITE_BASE_PATH || '/',
+    base: process.env.VITE_BASE_PATH ||  (isDev ? '/' : '/webrtc_client'),
     server: {
       proxy: {
         '/api': {
